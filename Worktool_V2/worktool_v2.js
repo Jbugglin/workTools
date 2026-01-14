646711fb12f2b1f08cb5d3751ac8a8236be46651
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2026-01-14 16:15:31 -0500

    Redesign forms and velocity calculator UI/logic
    Refactored HTML to use semantic fieldsets and legends, split forms for velocity and roll info, and updated class names for clarity. Enhanced CSS for improved layout and styling of forms, inputs, and buttons. Rewrote JavaScript to support the new velocity calculation form, added event listeners, and modularized calculation and display logic.

--- Worktool_V2/worktool_v2.js
+++ Worktool_V2/worktool_v2.js
@@ -1,28 +1,51 @@
-let form = document.getElementById('jobForm');
-let jobNo = document.getElementById('jobNo');
-let jobNum = "";
+/**
+ *  Calculate time for roll changes (based on 20 minutes per), 
+ *      and gap run outs (based on 5 min per)
+ */
+
+
+
+/**
+ *  Calculate Velocity to meet Estimated runtime for job.
+ */
 let pieceCount = document.getElementById('pieceCount');
 let totalPieceCount = 0;
 let estimatedRunTime = document.getElementById('estRun');
 let estRun = 0;
+let piecePerSheet = document.getElementById('piecePerSheet')
+let ppsVal = 0;
+let calculatedVelo = 0;
 
-function getJobNumber(){
-    jobNum = jobNo.value;
-    console.log("Job Number: " +jobNum);
+function clearVeloDisp(){
+    document.getElementById('velo-disp').innerHTML = "";
+}
+
+function dispVelo(){
+    document.getElementById('velo-disp').innerHTML = calculatedVelo+ " m/min";
+}
+
+function calcVelo(){
+    calculatedVelo = Math.ceil(((totalPieceCount / ppsVal) / estRun));
+}
+
+function getPiecePerSheet(){
+    ppsVal = piecePerSheet.value;
 }
 
 function getEstRun(){
     estRun = estimatedRunTime.value;
-    console.log("Est Run: " +estRun);
 }
 
 function getPieceCount(){
     totalPieceCount = pieceCount.value;
-    console.log("Piece count: " +totalPieceCount);
 }
 
-function jobDetailSubmit(){
-    getJobNumber();
+let veloForm = document.getElementById('calcVeloForm');
+veloForm.addEventListener("submit",(e) =>{
+    e.preventDefault();
     getPieceCount();
     getEstRun();
-}
+    getPiecePerSheet();
+    calcVelo();
+    dispVelo();
+});
